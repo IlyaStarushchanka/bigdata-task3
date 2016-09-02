8530cd0b16f046d1ea8294ca40385e4422c30a62
Ilya_Starushchanka <ilya10194@mail.ru>
2016-09-02 16:47:30 +0300

    Add cache for stopWords file

--- src/main/java/com/epam/bigdata/mapreduce/TagsCount.java
+++ src/main/java/com/epam/bigdata/mapreduce/TagsCount.java
@@ -1,6 +1,7 @@
 package com.epam.bigdata.mapreduce;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -12,6 +13,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -29,6 +32,19 @@ public class TagsCount {
         private Text word = new Text();
         private Set stopWords = new HashSet();
 
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            try{
+                Path[] stopWordsFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+                if(stopWordsFiles != null && stopWordsFiles.length > 0) {
+                    for(Path stopWordFile : stopWordsFiles) {
+                        readFile(stopWordFile);
+                    }
+                }
+            } catch(IOException ex) {
+                System.err.println("Exception in mapper setup: " + ex.getMessage());
+            }
+        }
 
         @Override
         protected void map(LongWritable key, Text value,
@@ -53,6 +69,19 @@ public class TagsCount {
             Matcher m = p.matcher(line);
             return m.matches();
         }
+
+        private void readFile(Path filePath) {
+            try{
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath.toString()));
+                String stopWord = null;
+                while((stopWord = bufferedReader.readLine()) != null) {
+                    stopWords.add(stopWord.toLowerCase());
+                }
+            } catch(IOException ex) {
+                System.err.println("Exception while reading stop words file: " + ex.getMessage());
+            }
+        }
+
     }
 
     public static class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable>{
@@ -92,7 +121,7 @@ public class TagsCount {
         job.setOutputValueClass(IntWritable.class);
 
         if (otherArgs.length > 2) {
-            job.addCacheFile(new Path(otherArgs[2]).toUri());
+            DistributedCache.addCacheFile(new Path(otherArgs[2]).toUri(), job.getConfiguration());
         }
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
