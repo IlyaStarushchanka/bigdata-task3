570167de3fa927794db48317eba64f2d135f7b9e
Ilya_Starushchanka <ilya10194@mail.ru>
2016-09-04 16:40:02 +0300

    User Agent

--- src/main/java/com/epam/bigdata/mapreduce/VisitsAndSpendsCount.java
+++ src/main/java/com/epam/bigdata/mapreduce/VisitsAndSpendsCount.java
@@ -45,10 +45,13 @@ public class VisitsAndSpendsCount {
                 visitsAndSpendsWritable.setSpendsCount(Integer.parseInt(columns[columns.length - 4]));
                 context.write(ip, visitsAndSpendsWritable);
             }
-            Pattern p2 = Pattern.compile("(?<=(\\w+\\s\\d+\\s\\w+\\s)).*(?=(\\d+[.]){3,}(\\d+|\\*))");
+            Pattern p2 = Pattern.compile("(?<=(\\w+\\s\\d+\\s\\w+\\s)).*(?=(\\s\\d+[.]){3,}(\\d+|\\*))");
             Matcher m2 = p2.matcher(line);
             if (m2.find()) {
-                System.out.println(m2.group());
+                String result = m2.group();
+                UserAgent userAgent = UserAgent.parseUserAgentString(result);
+                System.out.println(result);
+                System.out.println("User Agent - " + userAgent.getBrowser());
             }
             //UserAgent userAgent = UserAgent.parseUserAgentString(line);
             //System.out.println("User Agent - " + userAgent.getBrowser());
